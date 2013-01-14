8fe46bbe63adec5575fa13c4e389bc77ca31f0de
Peter Lundgren <peter@peterlundgren.com>
2013-01-13 20:28:31 -0500

    Use vim as manpager
    Inspired by Zameer Manji's blog post [1].
    
        [1]: http://zameermanji.com/blog/2012/12/30/using-vim-as-manpager/

--- _vim/.zsh/vim.zsh
+++ _vim/.zsh/vim.zsh
@@ -1,2 +1,10 @@
 alias vi="vim"
 export EDITOR="/usr/bin/vim"
+
+# Use vim as manpager
+#
+# The options set the file-type, set the tabstop, disable listchars, dispable
+# line numbers, and prevent modifying the file respectively. The command is
+# wrapped in `sh -c` because you can't use pipes in PAGER or MANPAGER on some
+# systems.
+export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nolist nonu noma nomod ' -\""
