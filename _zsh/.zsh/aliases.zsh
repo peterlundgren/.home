ee3b281861a5869cf692f2d33650b67042d35b68
Peter Lundgren <peter@peterlundgren.com>
2013-07-24 11:24:17 -0400

    Fix . alias to behave like builtin when arguments are provided
    The previous alias broke scripts that both:
    
    *   Used the . command
    *   Expected to be sourced with . or source themselves

--- _zsh/.zsh/aliases.zsh
+++ _zsh/.zsh/aliases.zsh
@@ -1,9 +1,25 @@
 alias rezshrc="source ~/.zshrc"
-alias .="cd .."
+
+# 'cd ..' if no arguments are provided. Behave like . or sorce otherwise.
+#
+# The . or source builtin requires at least 1 argument. Overload the no
+# argument version to 'cd ..' instead.
+dot() {
+    file=$1
+    if [ $# -gt 0 ]
+    then
+        shift
+        source $file
+    else
+        cd ..
+    fi
+}
+alias .=dot
 alias ..="cd ../.."
 alias ...="cd ../../.."
 alias ....="cd ../../../.."
 alias .....="cd ../../../../.."
+
 alias ls="ls --color=auto -F"
 alias ll="ls --color=auto -lF"
 alias la="ls --color=auto -aF"
