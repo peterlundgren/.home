4fbf840627afb0bee8053cb7bba23c6bea60186f
Peter Lundgren <peter@peterlundgren.com>
2014-05-12 13:54:01 -0400

    Search history with up and down keys

--- _zsh/.zsh/history.zsh
+++ _zsh/.zsh/history.zsh
@@ -5,3 +5,11 @@ setopt INC_APPEND_HISTORY
 setopt EXTENDED_HISTORY
 setopt HIST_IGNORE_DUPS
 setopt HIST_REDUCE_BLANKS
+
+# Search history with up and down keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
